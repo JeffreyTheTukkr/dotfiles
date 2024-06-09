20638f5ed5f87afc5ca14d21d9478803ca0e6539
Jeffrey <jeff.boesenkool@gmail.com>
2024-06-09 22:10:17 +0200

    update abbr

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -3,31 +3,22 @@ abbr -a c clear
 abbr -a h history
 abbr -a e exit
 
-# dnf
-abbr -a sdu sudo dnf update --refresh
-abbr -a sdr sudo dnf autoremove
-
 # various
+abbr -a vi vim
 abbr -a npp pnpm
-abbr -a nv nvim
+abbr -a ls lsd
+abbr -a l lsd -la
+abbr -a cat bat
 
 # git
 abbr -a gs git status
 abbr -a gd git diff
 abbr -a gr git restore
+abbr -a grs git restore --staged
 abbr -a ga git add
 abbr -a gcm git commit -m
 abbr -a gp git push
 abbr -a grh git reset HEAD~1
-
-# docker
-abbr -a d docker
-abbr -a dl docker logs
-abbr -a dp docker ps
-abbr -a dpa docker ps --all
-abbr -a dc docker compose
-abbr -a dcu docker compose up
-abbr -a dcd docker compose up -d
 
 # podman
 abbr -a p podman
@@ -37,3 +28,4 @@ abbr -a ppa podman ps --all
 abbr -a pc podman-compose
 abbr -a pcu podman-compose up
 abbr -a pcp podman-compose up -d
+
