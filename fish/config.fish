004636430c986acff9c67d9e618596d50d592b98
Jeffrey <39796883+JeffreyTheTukkr@users.noreply.github.com>
2025-05-21 22:20:31 +0800

    add fuck command

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,15 @@ if status is-interactive
 and not set -q TMUX
     exec tmux
 end
+
+function fuck
+    set -l last_cmd (history | head -n1 | string trim)
+
+    if test -z "$last_cmd"
+        echo "No previous command found."
+        return 1
+    end
+
+    echo "Running with sudo: $last_cmd"
+    eval "sudo $last_cmd"
+end
