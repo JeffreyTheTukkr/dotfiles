7177e2fe85e3166becad7d0d006b9e9f66868595
Jeffrey <39796883+JeffreyTheTukkr@users.noreply.github.com>
2026-04-25 22:08:26 +0800

    chore: remove fuck command

--- fish/config.fish
+++ fish/config.fish
@@ -27,15 +27,9 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 
-# run last command with sudo
-function fuck
-    set -l last_cmd (history | head -n1 | string trim)
+# fix psql issue
+fish_add_path /opt/homebrew/opt/postgresql/bin
 
-    if test -z "$last_cmd"
-        echo "No previous command found."
-        return 1
-    end
-
-    echo "> sudo $last_cmd"
-    sudo $last_cmd
-end
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
